f801f183dc5663544ab3fd9127fd0c50d3037f77
Sam O'Connor <sam@octech.com.au>
2016-02-26 11:11:11 +1100

    initial commmit

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
 # AWSSES
 
-[![Build Status](https://travis-ci.org/samoconnor/AWSSES.jl.svg?branch=master)](https://travis-ci.org/samoconnor/AWSSES.jl)
+AWS SES Interface for Julia
+
+[![Build Status](https://travis-ci.org/samoconnor/AWSSES.jl.svg)](https://travis-ci.org/samoconnor/AWSSES.jl)
+
+```julia
+using AWSSES
+
+aws = AWSCore.aws_config()
+
+AWSSES.ses_send(
+    aws,
+    to="sam@octe.ch",
+    from="sam@octe.ch",
+    subject="Hello",
+    body="Test")
+
+AWSSES.ses_send_attachments(
+    aws,
+    to="sam@octe.ch",
+    from="sam@octe.ch",
+    subject="hello",
+    body="test\n",
+    attachments=[("bar.txt", "text/plain", "bar\n"),
+                 ("foo.txt", "text/plain", "foo\n")])
+```
--- src/AWSSES.jl
+++ src/AWSSES.jl
@@ -1,5 +1,84 @@
+#==============================================================================#
+# AWSSES.jl
+#
+# SES API. See http://aws.amazon.com/documentation/ses/
+#
+# Copyright Sam O'Connor 2016 - All rights reserved
+#==============================================================================#
+
+
+__precompile__()
+
+
 module AWSSES
 
-# package code goes here
+export ses_send, ses_send_raw, ses_send_attachments
 
-end # module
+
+using AWSCore
+using SymDict
+using Retry
+
+
+
+function ses(aws, query)
+
+    query["AWSAccessKeyId"] = aws[:creds].secret_key
+
+    if aws[:creds].token != ""
+        query["SecurityToken"] = aws[:creds].token
+    end
+
+    do_request(post_request(aws, "email", "", query))
+end
+
+
+function ses_send(aws; to="", from="", subject="", body="")
+
+    ses(aws, Dict(
+        "Action" => "SendEmail",
+        "Source" => from,
+        "Destination.ToAddresses.member.1" => to,
+        "Message.Subject.Data" => subject,
+        "Message.Body.Text.Data" => body))
+end
+
+
+function ses_send_raw(aws; to="", from="", raw="")
+
+    ses(aws, Dict(
+        "Action" => "SendRawEmail",
+        "Source" => from,
+        "Destination.ToAddresses.member.1" => to,
+        "RawMessage.Data" => base64encode(raw)))
+end
+
+
+#    e.g. attachments=[("bar.txt", "text/plain", "bar\n"),
+#                      ("foo.txt", "text/plain", "foo\n")])
+
+function  ses_send_attachments(aws; to="", from="", subject="", body="",
+                                    attachments=[])
+
+    ses_send_raw(
+        aws,
+        to=to,
+        from=from,
+        raw = AWSCore.mime_multipart(
+            """
+            To: $to
+            From: $from
+            Subject: $subject
+            """,
+            [("", "text/plain", body), attachments...]))
+end
+
+
+
+end # module AWSSES
+
+
+
+#==============================================================================#
+# End of file.
+#==============================================================================#
